574fbb50557c24293dedd26ec60de636db183c8e
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2024-12-12 23:28:29 +0800

    feature: finished auto_deref

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,39 @@
 // proc macro crate
+mod auto;
 mod enum_from;
 mod enum_from_darling;
 
+use auto::{process_auto_debug, process_auto_deref};
 use enum_from::process_enum_from;
 use enum_from_darling::process_enum_from_darling;
 use proc_macro::TokenStream;
+use syn::DeriveInput;
 
 /// For enums, we want to generate From implementations for each variant.
 #[proc_macro_derive(EnumFrom)]
 pub fn derive_enum_from(input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let input = syn::parse_macro_input!(input as DeriveInput);
     // println!("input: {:#?}", input);
     process_enum_from(input).into()
 }
 
 #[proc_macro_derive(EnumFromDarling)]
 pub fn derive_enum_from_darling(input: TokenStream) -> TokenStream {
-    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let input = syn::parse_macro_input!(input as DeriveInput);
     // println!("input: {:#?}", input);
     process_enum_from_darling(input).into()
+}
+
+#[proc_macro_derive(AutoDeref, attributes(deref))]
+pub fn derive_auto_deref(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    // println!("input: {:#?}", input);
+    process_auto_deref(input).into()
+}
+
+#[proc_macro_derive(AutoDebug, attributes(debug))]
+pub fn derive_auto_debug(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as DeriveInput);
+    // println!("input: {:#?}", input);
+    process_auto_debug(input).into()
 }
